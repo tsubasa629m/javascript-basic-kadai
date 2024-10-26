11feca32fb92a0d82feba51bfc7acf2d6d319357
tsubasa629m <tsubasa629m@gmail.com>
2024-10-26 18:47:57 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// 数値を二乗する関数
+const calculateSquare = (num) => {
+  // 数値を二乗してその結果を戻り値として返す
+  return num * num;
+};
+
+// 関数を使用して10の二乗を計算
+const result = calculateSquare(10);
+
+// 結果
+console.log(result); 
